44cffd7c43cb3f4be66ab30ea5cd8ab5fff9d32b
lovekang3344 <1492303864@qq.com>
2026-01-17 17:29:21 +0800

    修改部署错误

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  // base: '/SEU-BUS/',
+  base: '/SEU-BUS',
   // server: {
   //   port: 3000,
   //   open: true
